741f4318b4226c7dbb44c52f4b5713397733861e
Sebastian Imlay <sebastian.imlay@gmail.com>
2015-07-27 15:59:18 -0700

    Added reference to figaro library

--- analysis/src/main/scala/hw.scala
+++ analysis/src/main/scala/hw.scala
@@ -1,1 +1,9 @@
-object Hi { def main(args: Array[String]) = println("Hi!") }
+import com.cra.figaro.library.compound.If
+
+
+object Hi {
+  def main(args: Array[String]) = {
+    println(args.length)
+    println("Hi!")
+  }
+}
