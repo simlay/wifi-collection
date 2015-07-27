ba89520a2fec0bda4e854f4358428e3c707d3883
Philip Moss Robinson <probinson@localhost.localdomain>
2015-07-27 14:02:42 -0700

    update to new dataset link

--- data/getdata.sh
+++ data/getdata.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-wget http://ec2-54-69-189-127.us-west-2.compute.amazonaws.com/locationssids/_all_docs?include_docs=true
+# wget http://ec2-54-69-189-127.us-west-2.compute.amazonaws.com/locationssids/_all_docs?include_docs=true
+wget http://ec2-54-69-189-127.us-west-2.compute.amazonaws.com/simple_set/_all_docs?include_docs=true
 mv _all_docs?include_docs=true db_dump.json
 
