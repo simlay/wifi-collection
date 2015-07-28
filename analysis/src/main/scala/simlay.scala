0a06015c31fa59853408ab576c73c9fa61ce463e
Sebastian Imlay <sebastian.imlay@gmail.com>
2015-07-28 14:41:31 -0700

    Merge probinso's work into mine

--- analysis/src/main/scala/simlay.scala
+++ analysis/src/main/scala/simlay.scala
@@ -1,39 +1,58 @@
-import com.cra.figaro.library.compound.If
-import com.cra.figaro.language.Flip
-import com.cra.figaro.language.Element
-import com.cra.figaro.library.atomic.continuous.MultivariateNormal
-import com.github.tototoshi.csv.CSVReader
 import java.io.File
+import com.github.tototoshi.csv.CSVReader
 
-class Position (lat:Double, lon:Double, sigma:Double) {
+import com.cra.figaro.library.compound.If
+import com.cra.figaro.library.atomic.continuous.Normal
+import com.cra.figaro.algorithm.learning.GeneralizedEM
+import com.cra.figaro.library.atomic.continuous.MultivariateNormal
+import com.cra.figaro.language.Element
+import com.cra.figaro.language.Constant
+import com.cra.figaro.algorithm.factored.VariableElimination
 
-  val distribution = MultivariateNormal(
-    List(lat, lon),
-    List(List(sigma, 0.0), List(0.0, sigma))
-  )
+class simlayTransmitterLocationModel {
+
+  // Location of Self
+  val sLat : Element[Double] = Constant(0.0)
+  val sLon : Element[Double] = Constant(0.0)
+  val sRad : Element[Double] = Constant(0.0)
+
+  val radSPosition : Element[Double] = Normal(sRad, 1) // should sigma be one?
+
+  val latSPosition : Element[Double] = Normal(sLat, radSPosition)
+  val lonSPosition : Element[Double] = Normal(sLon, radSPosition)
+
+  // Radial distance from trasmitter
+  val tPow : Element[Double] = Constant(0.0)
+  def powerToRadius(d : Element[Double]) = Constant(d.value)
+
+  val tRad : Element[Double] = powerToRadius(tPow)
+  def observe(lat:Double, lon:Double, radius:Double) {
+    latSPosition.observe(lat)
+    lonSPosition.observe(lon)
+    radSPosition.observe(radius)
+  }
 }
 
-object Hi {
+
+object simlay {
+
 
   def main(args: Array[String]) = {
+    println("BEGIN!")
+    val model = new simlayTransmitterLocationModel()
+
     val reader = CSVReader.open(new File("tables.csv"))
     val lines = reader.all()
-    var line = lines(0)
-
-    val ssid_location = new Position(
-      line(2).toDouble,
-      line(3).toDouble,
-      line(4).toDouble*100
-    )
-
     for(line <- lines) {
-      new Position(
+
+      model.observe(
         line(2).toDouble,
         line(3).toDouble,
-        line(4).toDouble
+        line(4).toDouble*10
       )
     }
-    reader.close()
-    println(ssid_location.distribution)
+
+    println(VariableElimination.probability(model.latSPosition, lines(0)(2).toDouble))
+    println(VariableElimination.probability(model.lonSPosition, lines(0)(3).toDouble))
   }
 }
