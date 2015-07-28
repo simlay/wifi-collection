fc6bbdc4670d44f931eb1a5fba89dc45604389c2
pmoss.robinson@gmail.com <pmoss.robinson@gmail.com>
2015-07-28 13:50:34 -0700

    changining names to avoid collisions

--- analysis/src/main/scala/probinso.scala
+++ analysis/src/main/scala/probinso.scala
@@ -6,27 +6,34 @@ import com.cra.figaro.language.Element
 import com.cra.figaro.library.atomic.continuous.MultivariateNormal
 import com.cra.figaro.language.Constant
 
-class transmissionModel {
+class transmitterLocationModel {
 
   // Location of Self
   val sLat : Element[Double] = Constant(0.0)
   val sLon : Element[Double] = Constant(0.0)
   val sRad : Element[Double] = Constant(0.0)
 
-  val radSPosition : Element[Double] = Normal(sRad, 1)
+  val radSPosition : Element[Double] = Normal(sRad, 1) // should sigma be one?
 
   val latSPosition : Element[Double] = Normal(sLat, radSPosition)
   val lonSPosition : Element[Double] = Normal(sLon, radSPosition)
 
   // Radial distance from trasmitter
   val tPow : Element[Double] = Constant(0.0)
- 
-  def powerToRadisu(d : Double) = d
-  
+  def powerToRadius(d : Element[Double]) = Constant(d.value)
+
+  val tRad : Element[Double] = powerToRadius(tPow)
 }
 
 
-object hi {
+object probinso {
+
+  def assertEvidence(model: transmitterLocationModel) {
+    model.sLat.observe(4.4)
+    model.sLon.observe(5.5)
+    model.sRad.observe(10)
+  }
+
   def main(args: Array[String]) = {
     val reader = CSVReader.open(new File("tables.csv"))
 
