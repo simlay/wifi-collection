f4a502f09042a224829ee39022a25e02173e1e9b
Sebastian Imlay <sebastian.imlay@gmail.com>
2015-07-28 14:51:38 -0700

    Moved my model inside.

--- analysis/src/main/scala/simlay.scala
+++ analysis/src/main/scala/simlay.scala
@@ -9,37 +9,36 @@ import com.cra.figaro.language.Element
 import com.cra.figaro.language.Constant
 import com.cra.figaro.algorithm.factored.VariableElimination
 
-class simlayTransmitterLocationModel {
-
-  // Location of Self
-  val sLat : Element[Double] = Constant(0.0)
-  val sLon : Element[Double] = Constant(0.0)
-  val sRad : Element[Double] = Constant(0.0)
-
-  val radSPosition : Element[Double] = Normal(sRad, 1) // should sigma be one?
-
-  val latSPosition : Element[Double] = Normal(sLat, radSPosition)
-  val lonSPosition : Element[Double] = Normal(sLon, radSPosition)
-
-  // Radial distance from trasmitter
-  val tPow : Element[Double] = Constant(0.0)
-  def powerToRadius(d : Element[Double]) = Constant(d.value)
-
-  val tRad : Element[Double] = powerToRadius(tPow)
-  def observe(lat:Double, lon:Double, radius:Double) {
-    latSPosition.observe(lat)
-    lonSPosition.observe(lon)
-    radSPosition.observe(radius)
-  }
-}
 
 
 object simlay {
+  class transmitterLocationModel {
 
+    // Location of Self
+    val sLat : Element[Double] = Constant(0.0)
+    val sLon : Element[Double] = Constant(0.0)
+    val sRad : Element[Double] = Constant(0.0)
+
+    val radSPosition : Element[Double] = Normal(sRad, 1) // should sigma be one?
+
+    val latSPosition : Element[Double] = Normal(sLat, radSPosition)
+    val lonSPosition : Element[Double] = Normal(sLon, radSPosition)
+
+    // Radial distance from trasmitter
+    val tPow : Element[Double] = Constant(0.0)
+    def powerToRadius(d : Element[Double]) = Constant(d.value)
+
+    val tRad : Element[Double] = powerToRadius(tPow)
+    def observe(lat:Double, lon:Double, radius:Double) {
+      latSPosition.observe(lat)
+      lonSPosition.observe(lon)
+      radSPosition.observe(radius)
+    }
+  }
 
   def main(args: Array[String]) = {
     println("BEGIN!")
-    val model = new simlayTransmitterLocationModel()
+    val model = new transmitterLocationModel()
 
     val reader = CSVReader.open(new File("tables.csv"))
     val lines = reader.all()
