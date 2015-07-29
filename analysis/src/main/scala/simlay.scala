cf48d5800e6a5baed71b0f2c4747aea0f2f4ce71
Sebastian Imlay <sebastian.imlay@gmail.com>
2015-07-29 14:17:13 -0700

    May have fixed inferrence model

--- analysis/src/main/scala/simlay.scala
+++ analysis/src/main/scala/simlay.scala
@@ -1,6 +1,6 @@
 import java.io.File
 import com.github.tototoshi.csv.CSVReader
-import math.{sqrt, pow, log10, abs}
+import math.{sqrt, pow, log10, abs, sin, Pi}
 
 import com.cra.figaro.library.compound.{If}
 import com.cra.figaro.library.atomic.continuous.{Uniform, Normal, MultivariateNormal}
@@ -16,27 +16,49 @@ import com.cra.figaro.algorithm.sampling.Importance
 
 
 object simlay {
+  def deg2rad(deg:Double) = {
+    deg * (Pi/180.0)
+  }
+  def getDistance(
+    lat1: Double,
+    lon1: Double,
+    lat2: Double,
+    lon2: Double
+  ) = {
+    var R = 6371;
+    var dLat : Double = deg2rad(lat2-lat1);
+    var dLon : Double = deg2rad(lon2-lon1);
+    var a =
+      sin(dLat / 2) * sin(dLat / 2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2.0) * Math.sin(dLon / 2.0)
+      ;
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in meters
+    // println(lat1 + "," + lon1 + "," + lat2 + "," + lon2 + ":" + (d*1000))
+    d*1000; // For the meters
+  }
 
   class Transmitter(frequency:Double, initial_lat:Double, initial_lon:Double) {
 
-    val latitude  : Element[Double] = Normal(initial_lat, 0.01)//Uniform(-90, 90)
-    val longitude : Element[Double] = Normal(initial_lon, 0.01)//Uniform(-180, 180)
+    val latitude  : Element[Double] = Uniform(-90, 90)// Normal(initial_lat, 1)
+    val longitude : Element[Double] = Uniform(-180, 180) // Normal(initial_lon, 1)
 
+    // val latitude  : Element[Double] = Normal(initial_lat, 1)
+    // val longitude : Element[Double] = Normal(initial_lon, 1)
 
-    def distance (x1:Double, y1:Double, x2:Double, y2:Double) = {
-      sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2))
-    }
 
     def assertSample(sample_lat:Double, sample_lon:Double, sample_power:Double) {
       val power = Apply(
         latitude, longitude,
-        (x: Double, y: Double) =>
-          20*log10(distance(sample_lon, x, sample_lat, y)) + 20*log10(frequency) + 10
+        (y: Double, x: Double) =>
+          20*log10(getDistance(sample_lat, sample_lon, y, x)) + 20*log10(frequency) + 92
       )
 
-      val normalPower : Element[Double] = Normal(power, 10)
+      // val normalPower : Element[Double] = Uniform(Apply(power, (p:Double) => p - 10.0),Apply(power, (p:Double) => p + 10.0))
+      //val normalPower : Element[Double] = Normal(power, 0.1)
 
-      normalPower.addConstraint((d : Double) => pow(0.02, abs(sample_power - d)))
+      power.addConstraint((d : Double) => pow(0.2, abs(sample_power - d)))
 
     }
     def inferLocation = {
@@ -49,7 +71,7 @@ object simlay {
     }
 
     def inferLocationMH = {
-      val algorithm = MetropolisHastings(20000, ProposalScheme.default, latitude, longitude)
+      val algorithm = MetropolisHastings(2000000, ProposalScheme.default, latitude, longitude)
       algorithm.start()
       val likely_latitude = algorithm.expectation(latitude, (i: Double) => i)
 
@@ -79,7 +101,7 @@ object simlay {
     var lat = first_line(2).toDouble
     var lon = first_line(3).toDouble
     var power = 0.0
-    println("Creating transmiter at " + lat + ", " + lon + ", freq: " + frequency)
+    println("Creating transmiter at " + lat + "," + lon + ", freq: " + frequency)
     val transmitter = new Transmitter(frequency, lat, lon)
 
     for(line <- lines) {
@@ -90,7 +112,14 @@ object simlay {
 
     }
     // println(transmitter.inferLocation)
-    println(transmitter.inferLocationMH)
+    val infered_location = transmitter.inferLocationMH
+    println(infered_location)
+    println(getDistance(
+      infered_location._1,
+      infered_location._2,
+      lat,
+      lon
+    ))
 
   }
 }
