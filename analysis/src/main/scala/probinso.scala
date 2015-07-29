a57818fefc6d534bc7b727689f238a0299ee1e7a
pmoss.robinson@gmail.com <pmoss.robinson@gmail.com>
2015-07-29 14:28:37 -0700

    fixed distance equasion

--- analysis/src/main/scala/probinso.scala
+++ analysis/src/main/scala/probinso.scala
@@ -1,12 +1,27 @@
 import java.io.File
-import scala.math.{abs, log10, pow, sqrt}
+import scala.math.{abs, log10, pow, sqrt, toRadians, sin, cos, atan2}
 import com.cra.figaro.language.{Apply, Element, Constant}
 import com.cra.figaro.library.atomic.continuous.{Normal, Uniform}
 import com.github.tototoshi.csv.CSVReader
-import com.cra.figaro.algorithm.factored.beliefpropagation.MPEBeliefPropagation
-
+import com.cra.figaro.algorithm.sampling.{ProposalScheme, MetropolisHastings}
 
 class transmitterModel(frequency: Double) {
+  
+  def distance_function(
+      lat_a : Double, lon_a : Double,
+      lat_b : Double, lon_b : Double) = {
+    val R = 6371.0
+    val dLat = toRadians(lat_b - lat_a)
+    val dLon = toRadians(lon_b - lon_a)
+
+    val a = sin(dLat/2.0) * sin(dLat/2.0) +
+            cos(toRadians(lat_a)) * cos(toRadians(lat_b)) *
+            sin(dLon/2.0) * sin(dLon/2.0)
+
+    val c = 2 * atan2(sqrt(a), sqrt(1-a))
+    val d = R * c
+    d
+  }
 
   val xLat : Element[Double] = Uniform(-90.0, 90.0)
   val xLon : Element[Double] = Uniform(-180.0, 180.0)
@@ -18,7 +33,7 @@ class transmitterModel(frequency: Double) {
 
     val _dist  : Element[Double] = Apply(xLat, xLon, sLat, sLon,
       (xLat: Double, xLon: Double, sLat : Double, sLon : Double) =>
-      sqrt(pow(abs(xLat - sLat),2) + pow(abs(xLon - sLon),2))
+      distance_function(xLat, xLon, sLat, sLon)
       )
 
     val _power : Element[Double] = Apply(_dist,
@@ -31,12 +46,12 @@ class transmitterModel(frequency: Double) {
   }
 
   def infer = {
-     val algorithm = MPEBeliefPropagation(10)
+     val algorithm = MetropolisHastings(20000, ProposalScheme.default, xLat, xLon)
      algorithm.start()
-     val retLat = algorithm.mostLikelyValue(xLat)
-     val retLon = algorithm.mostLikelyValue(xLon)
+     val retLat = algorithm.expectation(xLat, (i: Double) => i)
+     val retLon = algorithm.expectation(xLon, (i: Double) => i)
      algorithm.stop()
-     (retLat, retLon)
+     List(retLat, retLon)
   }
 }
 
@@ -46,14 +61,15 @@ object probinso {
     val reader = CSVReader.open(new File("tables.csv"))
     var transmitters : Map[String, transmitterModel] = Map()
     for (line <- reader.all()) {
-      if (!(transmitters.contains(line(6).toString)))
+      println(line)
+      if (!(transmitters.contains(line(6))))
         transmitters += (line(6) -> new transmitterModel(line(7).toDouble))
 
       transmitters(line(6)).assertEvidence(line(2).toDouble, line(3).toDouble,  line(8).toDouble)
     }
 
     for ((key, value) <- transmitters)
-      println(value.infer)
+      println(key :: value.infer)
 
     println()
     println("Yo Dog!")
