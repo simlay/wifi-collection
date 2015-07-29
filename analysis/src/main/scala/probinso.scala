4dd4a87f148a9cee6dbbb4c45c1ec461178f8681
pmoss.robinson@gmail.com <pmoss.robinson@gmail.com>
2015-07-28 21:15:43 -0700

    I think that this is creating all the transmitters

--- analysis/src/main/scala/probinso.scala
+++ analysis/src/main/scala/probinso.scala
@@ -9,7 +9,6 @@ import com.cra.figaro.language.Apply
 import com.cra.figaro.language.Element
 import com.cra.figaro.library.atomic.continuous.Normal
 import com.cra.figaro.library.atomic.continuous.Uniform
-
 import com.github.tototoshi.csv.CSVReader
 
 
@@ -30,22 +29,28 @@ class transmitterModel(frequency: Double) {
   val power  : Element[Double] = Normal(_power, 5)
 
   def assertEvidence(lat : Double, lon : Double, p : Double) {
-    sLat.setConstraint( (d : Double) => pow(0.02, abs(lat - d)) )
-    sLat.setConstraint( (d : Double) => pow(0.02, abs(lat - d)) )
-    sLat.setConstraint( (d : Double) => pow(0.02, abs(lat - d)) )
+    sLat.addConstraint( (d : Double) => pow(0.02, abs(lat - d)) )
+    sLat.addConstraint( (d : Double) => pow(0.02, abs(lat - d)) )
+    sLat.addConstraint( (d : Double) => pow(0.02, abs(lat - d)) )
   }
 
   def infer = {
-    0.5
+    
   }
 }
-
 
 object probinso {
 
   def main(args: Array[String]) = {
     val reader = CSVReader.open(new File("tables.csv"))
-    for (line <- reader.all()) println(line)
+    var transmitters : Map[String, transmitterModel] = Map()
+    for (line <- reader.all()) {
+      println(line)
+      if (! transmitters.exists(_ == line(6)))
+        transmitters += (line(6) -> new transmitterModel(line(7).toDouble))
+
+      transmitters(line(6)).assertEvidence(line(2).toDouble, line(3).toDouble,  line(8).toDouble)
+    }
 
     println(reader.all())
     println("Yo Dog!")
