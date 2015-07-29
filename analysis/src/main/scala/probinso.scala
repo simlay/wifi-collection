832b609fa80d16bef6ae6a242a8def8795505e09
pmoss.robinson@gmail.com <pmoss.robinson@gmail.com>
2015-07-28 17:11:16 -0700

    Philip Code

--- analysis/src/main/scala/probinso.scala
+++ analysis/src/main/scala/probinso.scala
@@ -1,46 +1,35 @@
 import java.io.File
-
-import com.cra.figaro.language.Constant
 import com.cra.figaro.language.Element
-
-import com.cra.figaro.algorithm.learning.ExpectationMaximization
-
 import com.cra.figaro.library.atomic.continuous.Normal
-
 import com.github.tototoshi.csv.CSVReader
 import scala.math.log10
 import scala.math.pow
+import scala.math.abs
+import scala.math.sqrt
+import com.cra.figaro.language.Constant
+import com.cra.figaro.library.atomic.continuous.Uniform
+import com.cra.figaro.language.Apply
 
-class selfLocationModel {
 
-  // Location of Self
-  val sLat : Element[Double] = Constant(0.0)
-  val sLon : Element[Double] = Constant(0.0)
-  val sRad : Element[Double] = Constant(0.0)
+class transmitterModel(frequency: Double) {
 
-  val radSPosition : Element[Double] = Normal(sRad, 1) // should sigma be one?
+  val xLat, sLat : Element[Double] = Uniform(-90, 90)
+  val xLon, sLon : Element[Double] = Uniform(-180, 180)
 
-  val latSPosition : Element[Double] = Normal(sLat, radSPosition)
-  val lonSPosition : Element[Double] = Normal(sLon, radSPosition)
+  val dist  : Element[Double] = Apply(xLat, xLon, sLat, sLon,
+      (xLat: Double, xLon: Double, sLat : Double, sLon : Double) =>
+      sqrt(pow(abs(xLat - sLat),2) + pow(abs(xLon - sLon),2))
+      )
 
-  def assertEvidence(Latitude : Double, Longitude : Double, Radius : Double) {
-    sLat.observe(Latitude)
-    sLon.observe(Longitude)
-    sRad.observe(Radius)
-  }
+
+
 }
 
-class transmitterRadiusModel(frequency: Double) {
-  // Radial distance from trasmitter
-  val tPow : Element[Double] = Constant(0.0) // should probably add noise
-
-  def powerToRadius(power_level : Element[Double]) = {
-    val k : Double = 0.0
-    pow(-10.0 - power_level.value - k -20*log10(frequency)/20.0,10.0)
-  }
-
-  val tRadius : Element[Double] = Constant(powerToRadius(tPow))
-
+class transmitterLocationModel(Latitude : Double, Longitude : Double, 
+    Radius : Double, Powers : List[Double]) {
+  val self = new selfLocationModel
+  self.assertEvidence(Latitude, Longitude, Radius)
+  
 }
 
 object probinso {
@@ -48,7 +37,6 @@ object probinso {
   def main(args: Array[String]) = {
     val reader = CSVReader.open(new File("tables.csv"))
     for (line <- reader.all()) println(line)
-        
 
     println(reader.all())
     println("Yo Dog!")
