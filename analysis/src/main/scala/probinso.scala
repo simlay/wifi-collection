d113829ff38503e084f298dcaf8190c67428ff37
Philip Moser <pmoser@localhost.localdomain>
2015-07-28 23:25:45 -0700

    fixing errors associated with pairing location to power

--- analysis/src/main/scala/probinso.scala
+++ analysis/src/main/scala/probinso.scala
@@ -1,41 +1,42 @@
 import java.io.File
 
-import scala.math.abs
-import scala.math.log10
-import scala.math.pow
-import scala.math.sqrt
+import scala.math.{abs, log10, pow, sqrt}
 
 import com.cra.figaro.language.Apply
 import com.cra.figaro.language.Element
 import com.cra.figaro.library.atomic.continuous.Normal
 import com.cra.figaro.library.atomic.continuous.Uniform
+
 import com.github.tototoshi.csv.CSVReader
 
 
 class transmitterModel(frequency: Double) {
 
-  val xLat, sLat : Element[Double] = Uniform(-90.0, 90.0)
-  val xLon, sLon : Element[Double] = Uniform(-180.0, 180.0)
+  val xLat : Element[Double] = Uniform(-90.0, 90.0)
+  val xLon : Element[Double] = Uniform(-180.0, 180.0)
 
-  val _dist  : Element[Double] = Apply(xLat, xLon, sLat, sLon,
+  def assertEvidence(lat : Double, lon : Double, p : Double) {
+
+    val _dist  : Element[Double] = Apply(xLat, xLon, sLat, sLon,
       (xLat: Double, xLon: Double, sLat : Double, sLon : Double) =>
       sqrt(pow(abs(xLat - sLat),2) + pow(abs(xLon - sLon),2))
       )
 
-  val _power : Element[Double] = Apply(_dist,
+    val _power : Element[Double] = Apply(_dist,
       ((dist : Double) => + 20 * log10(frequency) + 100)
       )
 
-  val power  : Element[Double] = Normal(_power, 5)
+    val power  : Element[Double] = Normal(_power, 5)
 
-  def assertEvidence(lat : Double, lon : Double, p : Double) {
-    sLat.addConstraint( (d : Double) => pow(0.02, abs(lat - d)) )
-    sLon.addConstraint( (d : Double) => pow(0.02, abs(lon - d)) )
     power.addConstraint( (d : Double) => pow(0.02, abs(p - d)) )
   }
 
   def infer = {
-    
+     val algorithm = MPEBeliefPropagation(10)
+     algorithm.start()
+     retval = algorithm.mostLikelyValue(xLat, xLon)
+     algorithm.stop()
+     retval
   }
 }
 
