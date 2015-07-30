6d68215417a5ccb3aeba5a44bfaaa68fe311ca6e
Daniel Salvadori <danaugrs@gmail.com>
2015-07-30 10:58:03 -0700

    Optimized visualization, included heatmaps.

--- visualize/index.js
+++ visualize/index.js
@@ -15,6 +15,7 @@ var rects = [];
 var heatMapData = [];
 var heatmap;
 
+var bounds;
 
 var circles = [];
 var markers = [];
@@ -98,8 +99,8 @@ function toggleCircleVisibility() {
 
 function setCircleVisibility(b) {
   var i = 0;
-  for (i = 0; i < circles.length; i++) {
-    circles[i].setVisible(b);
+  for (i = 0; i < positions.length; i++) {
+    positions[i].circle.setVisible(b);
   }
 }
 
@@ -133,10 +134,10 @@ function normalizeOpacity() {
     var color_cap = 0.6;
     var rgb = hslToRgb(color_cap*(1-(rects[i].fillOpacity/max)), 0.5, 0.5);
 
-    heatMapData[i].weight = rects[i].fillOpacity/max;
+    //heatMapData[i].weight = rects[i].fillOpacity/max;
 
     rects[i].setOptions({
-      fillOpacity: 0.3,// + rects[i].fillOpacity/(2*max),//rects[i].fillOpacity/max
+      fillOpacity: 0.5,// + rects[i].fillOpacity/(2*max),//rects[i].fillOpacity/max
       fillColor: rgbToHex(rgb[0], rgb[1], rgb[2])
     });
   }
@@ -151,7 +152,7 @@ function normalizeOpacity() {
 
   routers.push(marker);
 
-  heatmap.setMap(map); 
+  //heatmap.setMap(map); 
 }
 
 
@@ -163,7 +164,7 @@ function calcTotalProb(lat, lon) {
   for (i = 0; i < selected_positions.length; i++) {
     
     //var point = new google.maps.LatLng(selected_positions[i].latitude, selected_positions[i].longitude);
-    var distance = getDistanceFromLatLonInM(lat, lon, selected_positions[i].posdict.latitude, selected_positions[i].posdict.longitude);
+    var distance = getDistanceFromLatLonInM(lat, lon, selected_positions[i].latitude, selected_positions[i].longitude);
     //console.log("distance: ", distance)
 
 
@@ -207,11 +208,52 @@ function hslToRgb(h, s, l){
 
 
 
-function setupRectGrid(startLat, startLon, endLat, endLon) {
+function setupRectGrid(latLonBounds) {//startLat, startLon, endLat, endLon) {
 
+  while (rects[0]){
+    rects.pop().setMap(null);
+  }
+
+  /*
+  var r = new google.maps.Rectangle({
+        strokeColor: '#0000FF',
+        strokeOpacity: 0.0,
+        strokeWeight: 2,
+        fillColor: '#0000FF',
+        fillOpacity: opacity,
+        map: map,
+        bounds: latLonBounds,
+        zIndex: -10
+      });
+
+      rects.push(r);
+  */
+
+  //return; 
+
+  var NE = latLonBounds.getNorthEast();
+  var SW = latLonBounds.getSouthWest();
 
   var stepsLat = 100;
   var stepsLon = 100;
+
+  var startLat = NE.lat();
+  var startLon = SW.lng();
+
+  var endLat = SW.lat();
+  var endLon = NE.lng();
+
+  console.log(startLat, startLon, endLat, endLon);
+
+  //startLat = 45.580789;
+  //startLon = -122.573943;
+  //endLat = 45.578496;
+  //endLon = -122.570550;
+
+
+//console.log(startLat, startLon, endLat, endLon);
+
+  console.log("dist", getDistanceFromLatLonInM(NE.G, NE.K, SW.G, SW.K));
 
   var i = 0;
   for (i = 0; i < stepsLat; i++) {
@@ -219,9 +261,16 @@ function setupRectGrid(startLat, startLon, endLat, endLon) {
     var j = 0;
     for (j = 0; j < stepsLon; j++) {
 
-      var lat =  ((startLat + ((endLat - startLat)/stepsLat) * i) + (startLat + ((endLat - startLat)/stepsLat) * (i+1))) / 2;
-      var lon = ((startLon + ((endLon - startLon)/stepsLon) * j) + (startLon + ((endLon - startLon)/stepsLon) * (j+1))) / 2;
+      var lat =  ((startLat + ((endLat-startLat)/stepsLat) * i) + (startLat + ((endLat-startLat)/stepsLat) * (i+1))) / 2;
+      var lon = ((startLon + ((endLon-startLon)/stepsLon) * j) + (startLon + ((endLon-startLon)/stepsLon) * (j+1))) / 2;
       var opacity = getDistanceFromLatLonInM(lat, lon, 45.5798, -122.5723)/200;
+
+      var bnds = new google.maps.LatLngBounds(
+          new google.maps.LatLng( (startLat + ((endLat-startLat)/stepsLat) * i),
+                                  (startLon + ((endLon-startLon)/stepsLon) * j)),
+          new google.maps.LatLng( (startLat + ((endLat-startLat)/stepsLat) * (i+1)),
+                                  startLon + ((endLon-startLon)/stepsLon) * (j+1))
+          )
 
       var r = new google.maps.Rectangle({
         strokeColor: '#0000FF',
@@ -232,27 +281,43 @@ function setupRectGrid(startLat, startLon, endLat, endLon) {
         lat: lat,
         lon: lon,
         map: map,
-        bounds: new google.maps.LatLngBounds(
-          new google.maps.LatLng( startLat + ((endLat - startLat)/stepsLat) * i,
-                                  startLon + ((endLon - startLon)/stepsLon) * j),
-          new google.maps.LatLng( startLat + ((endLat - startLat)/stepsLat) * (i+1),
-                                  startLon + ((endLon - startLon)/stepsLon) * (j+1))
-          ),
+        bounds: bnds,
         zIndex: -10
       });
 
       rects.push(r);
 
-      var hmpoint = {location: new google.maps.LatLng(lat, lon), weight: 0.1}
-      heatMapData.push(hmpoint);
+      //var hmpoint = {location: new google.maps.LatLng(lat, lon), weight: 0.1}
+      //heatMapData.push(hmpoint);
 
 
 
 
-
-        }
 
       }
+
+    }
+
+
+
+    
+    var i = 0;
+    for (i = 0; i < rects.length; i++) {
+
+      var tp = calcTotalProb(rects[i].lat, rects[i].lon);
+
+      //heatMapData[i].weight = tp;
+
+      rects[i].setOptions({
+        fillOpacity: tp
+        //fillColor: "#FF0000"//hslToRgb(tp, 0.5, 0.5)
+
+
+      });
+    }
+
+    normalizeOpacity();
+    
 
 
 
@@ -366,16 +431,22 @@ function handleSelected(selected_hotspot) {
   while (routers[0]){
     routers.pop().setMap(null);
   }
-  //while (circles[0]){
-  //  circles.pop().setMap(null);
-  //}
+  while (rects[0]){
+    rects.pop().setMap(null);
+  }
 
   // Clear selected positions
   selected_positions = [];
 
+  bounds = new google.maps.LatLngBounds();
+  var max_radius = 0;
+
   // Render accuracy if requested
   if (selected_hotspot == "all") {
     $.each(positions, function(index, pos) {
+
+      bounds.extend(pos.marker.position);
+
 
       hotspots_sample = pos.ssids;
 
@@ -391,6 +462,11 @@ function handleSelected(selected_hotspot) {
 
       pos.circle.setVisible(true);
 
+
+      if (pos.accuracy > max_radius) {
+        max_radius = pos.accuracy;
+      }
+
     });
   }
 
@@ -403,24 +479,38 @@ function handleSelected(selected_hotspot) {
       pos.circle.setVisible(false);
     });
 
+    
+
     // Then show the positions markers and circles related to hotspot
     $.each(positions, function(index, pos) {
 
       hotspots_sample = pos.ssids;
-      point = new google.maps.LatLng(position_dict.latitude, position_dict.longitude);
+      //point = new google.maps.LatLng(position_dict.latitude, position_dict.longitude);
+
 
       var i;
       for (i = 0; i < hotspots_sample.length; i++) {
         if (hotspots_sample[i].BSSID == selected_hotspot) { // Not yet in all_hotspots array
 
+          selected_positions.push(pos);
+
+          bounds.extend(pos.marker.position);
+
           pos.marker.setVisible(true);
           pos.circle.setVisible(true);
+
+
+          var radius = Math.pow(10,-hotspots_sample[i].level/20)/600;
+          //console.log(pos.accuracy);
+          if (radius > max_radius) {
+            max_radius = radius;
+          }
 
           pos.circle.setOptions({
             _level: hotspots_sample[i].level,
             _freq: hotspots_sample[i].frequency,
 
-            radius: Math.pow(10,-hotspots_sample[i].level/20)/600,
+            radius: radius,
             map: map,
             fillColor: "#550000",
             strokeColor: "#880000",//stroke color,
@@ -430,6 +520,11 @@ function handleSelected(selected_hotspot) {
       }
 
     });
+
+ 
+
+
+    
 
     // Update colors
 
@@ -451,13 +546,34 @@ function handleSelected(selected_hotspot) {
 
     normalizeOpacity();
     */
+
+    
   }
+
+  
+  // Extend bound based on max_accuracy (lowest accuracy)
+  var pointNW = google.maps.geometry.spherical.computeOffset(bounds.getNorthEast(), max_radius, 45);//315);
+  var pointSE = google.maps.geometry.spherical.computeOffset(bounds.getSouthWest(), max_radius, 225);//135);
+
+  bounds.extend(pointNW);
+  bounds.extend(pointSE);
+
+  // Center map on markers
+  map.fitBounds(bounds);
+  //map.setZoom(map.getZoom()-1); 
+
+
 }
 
 
 // ===============================================================================================
 // Set up DAT.GUI
 // ===============================================================================================
+
+function showHeatMap() {
+  setupRectGrid(bounds);
+}
+
 
 window.onload = function() {
 
@@ -479,9 +595,10 @@ window.onload = function() {
 
   gui.add(this, 'toggleCircleVisibility').name("Toggle Circles");
   gui.add(this, 'toggleRectVisibility').name("Toggle Rect HM");
+   gui.add(this, 'showHeatMap').name("Show Heatmap");
     
 
-  
+  $("#spinner").hide();
 
   // Setup JQuery Widgets
 
@@ -511,24 +628,27 @@ window.onload = function() {
     $( "#hotspots" ).autocomplete( "search", " " );
   })
 
+  $("#heatmap").click(function() {
+    $("#spinner").show();
+
+    setTimeout(function () {
+        showHeatMap();
+        $("#spinner").hide();
+    }, 2000);
+    
+    //$( "#hotspots" ).focus();
+    //$( "#hotspots" ).autocomplete( "search", " " );
+  })
+
   $('input').addClass("ui-corner-all");
-
   $('input').button();
-
   $( "#accordion" ).accordion();
+  $( "button" ).button();
+  $( "radioset" ).buttonset();
 
+  $( "tabs" ).tabs();
 
-
-  $( "#button" ).button();
-  $( "#radioset" ).buttonset();
-
-
-
-  $( "#tabs" ).tabs();
-
-
-
-  $( "#dialog" ).dialog({
+  $( "dialog" ).dialog({
     autoOpen: false,
     width: 400,
     buttons: [
@@ -548,49 +668,32 @@ window.onload = function() {
   });
 
   // Link to open the dialog
-  $( "#dialog-link" ).click(function( event ) {
+  $( "dialog-link" ).click(function( event ) {
     $( "#dialog" ).dialog( "open" );
     event.preventDefault();
   });
 
-
-
-  $( "#datepicker" ).datepicker({
+  $( "datepicker" ).datepicker({
     inline: true
   });
 
-
-
-  $( "#slider" ).slider({
+  $( "slider" ).slider({
     range: true,
     values: [ 17, 67 ]
   });
 
-
-
-  $( "#progressbar" ).progressbar({
+  $( "progressbar" ).progressbar({
     value: 20
   });
 
-
-
-  $( "#spinner" ).spinner();
-
-
-
-  $( "#menu" ).menu();
-
-
-
-  $( "#tooltip" ).tooltip();
-
-
-
-  $( "#selectmenu" ).selectmenu();
+  $( "spinner" ).spinner();
+  $( "menu" ).menu();
+  $( "tooltip" ).tooltip();
+  $( "selectmenu" ).selectmenu();
 
 
   // Hover states on the static widgets
-  $( "#dialog-link, #icons li" ).hover(
+  $( "dialog-link, #icons li" ).hover(
     function() {
       $( this ).addClass( "ui-state-hover" );
     },
@@ -600,7 +703,7 @@ window.onload = function() {
   );
 
 
-plotMap();
+  plotMap();
 
 };
 
@@ -617,7 +720,7 @@ function plotMap() {
       mapdata = data;
 
       map = new google.maps.Map(document.getElementById('map-canvas'));
-      var bounds = new google.maps.LatLngBounds();
+      bounds = new google.maps.LatLngBounds();
       
       //google.maps.event.addListener(map, 'zoom_changed', function () {
       //    heatmap.setOptions({radius:getNewRadius()});
@@ -640,7 +743,7 @@ function plotMap() {
               if ($.inArray(hotspots_sample[i].BSSID, all_hotspots_BSSIDs) == -1) { // Not yet in all_hotspots array
                   all_hotspots.push({"BSSID": hotspots_sample[i].BSSID, "SSID": hotspots_sample[i].SSID})
                   all_hotspots_names.push({
-                    "label": hotspots_sample[i].BSSID + " - " + hotspots_sample[i].SSID,
+                    "label": hotspots_sample[i].SSID + " (" + hotspots_sample[i].BSSID + ")",
                     "value": hotspots_sample[i].BSSID
                   });
               }
@@ -666,8 +769,6 @@ function plotMap() {
             infowindow.open(map,marker);
           });
 
-
-
           var circle = new google.maps.Circle({
               center: point,
               radius: position_dict.accuracy,
@@ -684,11 +785,21 @@ function plotMap() {
           position_dict.circle = circle;
 
           // Extend bound for centering
-          bounds.extend(marker.position);
+          bounds.extend(point);
 
           positions.push(position_dict);
 
       });
+
+      
+
+      // Extend bound based on max_accuracy (lowest accuracy)
+      //var pointNW = google.maps.geometry.spherical.computeOffset(bounds.getNorthEast(), 50, 45);//315);
+      //var pointSE = google.maps.geometry.spherical.computeOffset(bounds.getSouthWest(), 50, 225);//135);
+      // NOTE NorthEast and SouthWest are actually returning NW and SE.
+
+      //bounds.extend(pointNW);
+      //bounds.extend(pointSE);
 
       // Center map on markers
       map.fitBounds(bounds);
@@ -696,6 +807,7 @@ function plotMap() {
       //if (all_hotspots[i].BSSID == "c8:b3:73:4f:50:1a") {
   
       // Draw grid
+      //setupRectGrid(bounds);
       //setupRectGrid(45.580789, -122.573943, 45.578496, -122.570550);
       //setupRectGrid(45.574083, -122.559098, 45.571910, -122.556671);
       
@@ -724,7 +836,7 @@ function plotMap() {
       //heatmap.setMap(map);
   };
 
-  $.getJSON('simple_set.json', onSuccess);
+  $.getJSON('db_dump.json', onSuccess);
 
 }
 //google.maps.event.addDomListener(window, 'load', plotMap);
