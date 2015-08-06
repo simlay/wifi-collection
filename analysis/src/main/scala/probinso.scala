c7484a55db1f9de1ef1388f8c24385a5fc6a9217
pmoss.robinson@gmail.com <pmoss.robinson@gmail.com>
2015-08-05 17:17:19 -0700

    TODO related notes, and includded universes support for greater speedup

--- analysis/src/main/scala/probinso.scala
+++ analysis/src/main/scala/probinso.scala
@@ -1,6 +1,6 @@
 import java.io.File
 import scala.math.{abs, log10, pow, sqrt, toRadians, sin, cos, atan2}
-import com.cra.figaro.language.{Apply, Element, Constant}
+import com.cra.figaro.language.{Apply, Element, Constant, Universe}
 import com.cra.figaro.library.atomic.continuous.{Normal, Uniform}
 import com.github.tototoshi.csv.CSVReader
 import com.cra.figaro.algorithm.sampling.{ProposalScheme, MetropolisHastings}
@@ -9,14 +9,14 @@ import com.cra.figaro.algorithm.sampling.{ProposalScheme, MetropolisHastings}
 
 object probinso {
   class transmitterModel(frequency: Double) {
-    
+
     def distance_function(
         lat_a : Double, lon_a : Double,
         lat_b : Double, lon_b : Double) = {
       val R = 6371.0
       val dLat = toRadians(lat_b - lat_a)
       val dLon = toRadians(lon_b - lon_a)
-  
+
       val a = sin(dLat/2.0) * sin(dLat/2.0) +
               cos(toRadians(lat_a)) * cos(toRadians(lat_b)) *
               sin(dLon/2.0) * sin(dLon/2.0)
@@ -25,35 +25,39 @@ object probinso {
       val d = R * c
       d * 1000 // km -> m
     }
-  
+
+    val myUniverse = new Universe()
     val xLat : Element[Double] = Uniform(-90.0, 90.0)
     val xLon : Element[Double] = Uniform(-180.0, 180.0)
-  
+
+    //val xPow : Element[Double] = Normal(50, 10)
+
     def assertEvidence(lat : Double, lon : Double, p : Double) {
-  
+
       val sLat : Element[Double] = Constant(lat)
       val sLon : Element[Double] = Constant(lon)
-  
+
       val _dist  : Element[Double] = Apply(xLat, xLon, sLat, sLon,
         (xLat: Double, xLon: Double, sLat : Double, sLon : Double) =>
         distance_function(xLat, xLon, sLat, sLon)
         )
 
       val _power : Element[Double] = Apply(_dist,
-        ((dist : Double) => + 20 * log10(frequency) + 100)
+        ((dist : Double) => + 20 * log10(frequency) + 92)
         )
 
-      val power  : Element[Double] = Normal(_power, 5)
+      val power  : Element[Double] = Normal(_power, 1) // TODO : Justify sigma
 
       power.addConstraint((d : Double) => pow(0.02, abs(p - d)))
     }
-  
+
     def infer = {
-       val algorithm = MetropolisHastings(20000, ProposalScheme.default, xLat, xLon)
-       algorithm.start()
+       val algorithm = MetropolisHastings(2000000, ProposalScheme.default, xLat, xLon)(myUniverse)
+       algorithm.start
        val retLat = algorithm.expectation(xLat, (i: Double) => i)
        val retLon = algorithm.expectation(xLon, (i: Double) => i)
-       algorithm.stop()
+       algorithm.stop
+       algorithm.kill
        List(retLat, retLon)
     }
   }
@@ -66,6 +70,8 @@ object probinso {
       if (!(transmitters.contains(line(6))))
         transmitters += (line(6) -> new transmitterModel(line(7).toDouble))
 
+      // TODO : Use a 1:1 (transmitter -> universe) implementation for significant speedup
+      //        Presently for every infer all transmitters are being processed over
       transmitters(line(6)).assertEvidence(line(2).toDouble, line(3).toDouble,  line(8).toDouble)
     }
 
